04a875112a46f8b98308ad34d8f3b12473ef60fb
viet-ho <103388731+viet-ho@users.noreply.github.com>
2023-11-18 12:51:17 -0700

    Update App.js regarding to the last commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import ReviewsPage from "./components/ReviewsPage";
 import SeatBookingPage from "./components/SeatBookingPage";
 import PaymentPage from "./components/PaymentPage";
 import TicketBookingPage from "./components/TicketBookingPage";
-import ConfirmationPage from "./components/ConfirmationPage";
 import CreditCardPage from "./components/CreditCardPage";
+import SummaryPage from "./components/SummaryPage";
 
 function App() {
-  return <HomePage></HomePage>;
+  //return <HomePage></HomePage>;
   //return <ReviewsPage></ReviewsPage>;
   //return <SeatBookingPage></SeatBookingPage>;
-  return <PaymentPage></PaymentPage>;
+  //return <PaymentPage></PaymentPage>;
   //return <TicketBookingPage></TicketBookingPage>;
-  //return <ConfirmationPage></ConfirmationPage>;
   //return <CreditCardPage></CreditCardPage>;
+  return <SummaryPage></SummaryPage>;
+
 }
 
 export default App;
