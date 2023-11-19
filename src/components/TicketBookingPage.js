44d4d21adcfe31a017b0191fe6f377dd550eb75f
viet-ho <103388731+viet-ho@users.noreply.github.com>
2023-11-18 18:33:06 -0700

    Updated the design for ticket booking page

--- src/components/TicketBookingPage.js
+++ src/components/TicketBookingPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import "../styles/TicketBookingPage.css";
 import Modal from "../components/Modal.js";
 
@@ -27,45 +28,54 @@ function TicketBookingPage() {
         setShowModal(!showModal);
     };
 
+    useEffect(() => {
+        document.body.classList.add('ticket-page-background');
+
+        return () => {
+            document.body.classList.remove('ticket-page-background');
+        };
+    }, []);
+
     return (
-        <div className="container">
-            <Modal showModal={showModal} toggleModal={toggleModal} message={modalMessage} />
-            <h2>Ticket Type</h2>
+        <div className="ticket-page">
+            <div className="ticket-container">
+                <Modal showModal={showModal} toggleModal={toggleModal} message={modalMessage} />
+                <h2>Ticket Type</h2>
 
-            <div className="card mb-3">
-                <div className="card-body">
-                    <span>Adult (18-64): $22</span>
-                    <div>
-                        <button className="btn btn-primary" onClick={() => setAdultCount(adultCount + 1)}>+</button>
-                        <span className="mx-2">{adultCount}</span>
-                        <button className="btn btn-primary" onClick={() => adultCount > 0 && setAdultCount(adultCount - 1)}>-</button>
+                <div className="card mb-3">
+                    <div className="card-body">
+                        <span>Adult (18-64): $22</span>
+                        <div>
+                            <button className="btn btn-primary" onClick={() => setAdultCount(adultCount + 1)}>+</button>
+                            <span className="mx-2">{adultCount}</span>
+                            <button className="btn btn-primary" onClick={() => adultCount > 0 && setAdultCount(adultCount - 1)}>-</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="card mb-3">
-                <div className="card-body">
-                    <span>Child (0-17): $10</span>
-                    <div>
-                        <button className="btn btn-primary" onClick={() => setChildCount(childCount + 1)}>+</button>
-                        <span className="mx-2">{childCount}</span>
-                        <button className="btn btn-primary" onClick={() => childCount > 0 && setChildCount(childCount - 1)}>-</button>
+                <div className="card mb-3">
+                    <div className="card-body">
+                        <span>Child (0-17): $10</span>
+                        <div>
+                            <button className="btn btn-primary" onClick={() => setChildCount(childCount + 1)}>+</button>
+                            <span className="mx-2">{childCount}</span>
+                            <button className="btn btn-primary" onClick={() => childCount > 0 && setChildCount(childCount - 1)}>-</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="card mb-3">
-                <div className="card-body">
-                    <span>Senior (65+): $10</span>
-                    <div>
-                        <button className="btn btn-primary" onClick={() => setSeniorCount(seniorCount + 1)}>+</button>
-                        <span className="mx-2">{seniorCount}</span>
-                        <button className="btn btn-primary" onClick={() => seniorCount > 0 && setSeniorCount(seniorCount - 1)}>-</button>
+                <div className="card mb-3">
+                    <div className="card-body">
+                        <span>Senior (65+): $10</span>
+                        <div>
+                            <button className="btn btn-primary" onClick={() => setSeniorCount(seniorCount + 1)}>+</button>
+                            <span className="mx-2">{seniorCount}</span>
+                            <button className="btn btn-primary" onClick={() => seniorCount > 0 && setSeniorCount(seniorCount - 1)}>-</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/*
+                {/*
             <div className="mb-3">
                 <label>How many wheelchair seats will be required, if any?:</label>
                 <input
@@ -90,26 +100,31 @@ function TicketBookingPage() {
             </div>
                 */}
 
-            <div className="mb-3">
-                <label className="label-text">Special Assistance:</label>
-                <textarea
-                    className="form-control"
-                    rows="3"
-                    value={specialAssistance}
-                    onChange={(e) => setSpecialAssistance(e.target.value)}
-                    placeholder="Please provide details for any special assistance needed."
-                ></textarea>
-            </div>
+                <div className="mb-3">
+                    <label className="label-text">Special Assistance:</label>
+                    <textarea
+                        className="form-control"
+                        rows="3"
+                        value={specialAssistance}
+                        onChange={(e) => setSpecialAssistance(e.target.value)}
+                        placeholder="Please provide details for any special assistance needed."
+                    ></textarea>
+                </div>
 
-            <div className="subtotal-field">
-                <span>Subtotal: ${subtotal}</span>
-            </div>
+                <div className="subtotal-field">
+                    <span>Subtotal: ${subtotal}</span>
+                </div>
 
-            <div className="ticket-action-container">
-                <button className="btn btn-confirm" onClick={handleConfirmTickets}>Confirm Ticket(s)</button>
-                <button className="btn btn-back">Back</button>
-            </div>
+                <div className="ticket-action-container">
+                    <Button variant="primary" className="ticket-confirm-button" onClick={handleConfirmTickets}>
+                        Confirm Ticket(s)
+                    </Button>
+                    <Button variant="secondary" className="ticket-back-button">
+                        Back
+                    </Button>
+                </div>
 
+            </div>
         </div>
     );
 }
